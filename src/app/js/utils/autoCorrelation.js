2d85f4424301cb232839b733fad7999d7660eefb
Hauser <hauser@users.noreply.github.com>
2019-03-08 21:06:32 +0100

    Optimize auto correlation and add comments

--- src/app/js/utils/autoCorrelation.js
+++ src/app/js/utils/autoCorrelation.js
@@ -1,35 +1,71 @@
 const autoCorrelation = (a1, a2 = undefined) => {
-    let mx = [...a1];
-    let my = (a2) ? [...a2] : [...a1];
+    /*
+        Mult-matrix:
+
+              1 2  3  4 mx
+            4 4 8 12 16
+            3 3 6  9 12
+            2 2 4  4  8
+            1 1 2  3  4
+            my
+    */
 
     let matrix = [];
-    for (let y of my.reverse()) {
-        let row = [];
-        for (let x of mx) {
-            row.push(x * y);
+    {
+        const mx = a1; // No copy for optimization
+        const my = (a2) ? [...a2].reverse() : [...a1].reverse();
+        for (let y of my) {
+            let row = [];
+            for (let x of mx) {
+                row.push(x * y);
+            }
+            matrix.push(row);
         }
-        matrix.push(row);
     }
- 
-    const getSum = (x, y) => {
+
+    /*
+        Sum every step:
+
+            4 8 12  . x
+            3 6  . 12
+            2 .  4  8
+            . 2  3  4
+            y
+
+            matrix[down][left] 
+            matrix[y+1][x-1]
+    */
+
+    const getSum = (y, x) => {
         let sum = 0;
-        while (x < matrix.length && y >= 0) {
-            sum += matrix[x++][y--];
+        while (y < matrix.length && x >= 0) {
+            sum += matrix[y++][x--];
         }
         return sum;
     };
 
-    const xx = 0;
-    const yy = matrix[0].length - 1;
+    /*
+        Targets:
+
+            . . . .
+            3 6 9 .
+            2 4 4 .
+            1 2 3 .
+
+            matrix[0][x]
+            matrix[y][maxx]
+    */
+
+    const maxx = matrix[0].length - 1;
 
     let result = [];
-    for (let x = 0; x < matrix.length; ++x) {
-        for (let y = 0; y < matrix[x].length; ++y) {
-            if (x == xx || y == yy) {
-                result.push(getSum(x, y));
-            }
-        }
+    for (let x = 0; x < maxx; ++x) {
+        result.push(getSum(0, x));
     }
+    for (let y = 0; y < matrix.length; ++y) {
+        result.push(getSum(y, maxx));
+    }
+
     return result;
 };
 
